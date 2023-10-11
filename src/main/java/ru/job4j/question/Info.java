de480abe17cbc1135865e6038f99ed9c9f1f565f
s.starikov <s.starikov@gaskar.group>
2023-10-11 19:22:43 +0500

    3. Статистика по коллекции. [#45889 #451931]

--- src/main/java/ru/job4j/question/Info.java
+++ src/main/java/ru/job4j/question/Info.java
@@ -61,10 +61,9 @@ public class Info {
 
     @Override
     public String toString() {
-        return "Info{" +
-                "added=" + added +
-                ", changed=" + changed +
-                ", deleted=" + deleted +
-                '}';
+        return "Info{" + "added=" + added
+                + ", changed=" + changed
+                + ", deleted=" + deleted
+                + '}';
     }
 }
