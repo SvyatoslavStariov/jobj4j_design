f95034714eec548d4a2292f779f45a1080e2d5c6
s.starikov <s.starikov@gaskar.group>
2023-10-24 01:29:15 +0500

    Revert "8. Тестирование IO [#173905]"
    This reverts commit 8770821cbbcc7c132557b219c650c1c3e1d97c6b.

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class Analysis {
     public void unavailable(String source, String target) {
@@ -48,7 +47,7 @@ public class Analysis {
 
 
     private void writeFile(String rls, String target) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new PrintWriter(target))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(target))) {
             bufferedWriter.write(rls);
         } catch (IOException e) {
             e.printStackTrace();
