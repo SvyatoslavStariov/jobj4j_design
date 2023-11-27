8cd8d988f9875928596fef0e06531335c857f80c
s.starikov <s.starikov@gaskar.group>
2023-11-27 12:28:22 +0500

    2. Что такое Socket? [#4850 #462601]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -19,34 +19,11 @@ public class EchoServer {
                     String str = in.readLine();
                     System.out.println(str);
                     out.flush();
-                    if (isFinish(str)) {
+                    if (str.contains("msg=Bye")) {
                         server.close();
                     }
                 }
             }
         }
-    }
-
-    private static boolean isFinish(String str) {
-        String[] params = getParams(str);
-        boolean isFinish = false;
-        if (params.length != 0) {
-            String[] requestParams = params[1].split("=", 2);
-            String key = requestParams[0].replaceAll("[/?]", "");
-            String value = requestParams[1];
-            isFinish = "msg".equals(key) && "Bye".equals(value);
-        }
-        return isFinish;
-    }
-
-    private static String[] getParams(String str) {
-        String[] arr = new String[]{};
-        if (str.startsWith("GET")) {
-            String[] params = str.split(" ");
-            if (params.length > 1 && params[1].contains("=")) {
-                arr = params;
-            }
-        }
-        return arr;
     }
 }
