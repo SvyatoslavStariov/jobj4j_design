f8e57a39d3f12a3fecb91ebd3811586745a3ae1c
s.starikov <s.starikov@gaskar.group>
2023-11-29 15:18:53 +0500

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/xml/City.java
+++ src/main/java/ru/job4j/serialization/xml/City.java
@@ -1,9 +1,19 @@
 package ru.job4j.serialization.xml;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "city")
 public class City {
+
+    @XmlAttribute
     private String address;
 
+    @XmlAttribute
     private String inn;
+
+    public City() {
+    }
 
     public City(String address, String inn) {
         this.address = address;
--- src/main/java/ru/job4j/serialization/xml/Student.java
+++ src/main/java/ru/job4j/serialization/xml/Student.java
@@ -1,17 +1,34 @@
 package ru.job4j.serialization.xml;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.List;
 
+@XmlRootElement(name = "student")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Student {
+    @XmlAttribute
     private String name;
 
+    @XmlAttribute
     private int age;
 
+    @XmlAttribute
     private String sex;
 
     private City city;
 
     private List<String> subjects;
+
+    public Student() {
+    }
 
     public Student(String name, int age, String sex, City city, List<String> subjects) {
         this.name = name;
@@ -30,5 +47,23 @@ public class Student {
                 + ", city=" + city
                 + ", subjects=" + subjects
                 + '}';
+    }
+
+    public static void main(String[] args) throws Exception {
+        Student student = new Student("Андрей", 30, "Муж", new City("г.Москва", "4534534"), List.of("Биология", "Химия"));
+        JAXBContext context = JAXBContext.newInstance(Student.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(student, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Student result = (Student) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
     }
 }
