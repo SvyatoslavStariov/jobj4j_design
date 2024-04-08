9bb6b4594f0f91d0d55293cd1196d371c24f1a6a
s.starikov <s.starikov@gaskar.group>
2024-04-09 01:04:40 +0500

    2. Триггеры [#504803]

---
+++ db/product_trigger.sql
@@ -0,0 +1,37 @@
+create
+or replace function after_taxes()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price + price * 0.13;
+        return new;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger after_taxes_trigger
+    after insert
+    on products
+    referencing new table as
+                    inserted
+    for each statement
+    execute procedure after_taxes();
+
+create
+or replace function before_taxes()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price + price * 0.13;
+        return new;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger before_taxes_trigger
+    before insert
+    on products
+    for each row
+    execute procedure before_taxes();
