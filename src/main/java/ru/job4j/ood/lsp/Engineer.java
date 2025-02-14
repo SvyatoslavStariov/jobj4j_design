48add2312324133f87da2c01450d69a6c8788ce8
s.starikov <s.starikov@gaskar.group>
2025-02-15 01:29:03 +0500

    0. Принцип подстановки Лисков. [#4915]

---
+++ src/main/java/ru/job4j/ood/lsp/Engineer.java
@@ -0,0 +1,12 @@
+package ru.job4j.ood.lsp;
+
+public abstract class Engineer {
+
+    public void drawProduct() {
+        return;
+    }
+
+    public String designsProduct() {
+        return "";
+    }
+}
